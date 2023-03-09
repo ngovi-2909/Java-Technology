9f5f124d14b527f0fdde087264dd019be49d4986
ngovi <74971162+ohwonwi@users.noreply.github.com>
2023-03-09 20:09:04 +0700

    Create HibernateUtils.java

---
+++ Lab5/src/main/java/tdtu/edu/utils/HibernateUtils.java
@@ -0,0 +1,28 @@
+package tdtu.edu.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import tdtu.edu.model.*;
+import java.util.Properties;
+
+public class HibernateUtils {
+    private static final SessionFactory FACTORY;
+    static{
+        Configuration conf=new Configuration();
+        Properties props = new Properties();
+        
+        conf.configure("hibernate.cfg.xml");
+        conf.addAnnotatedClass(User.class);//Khai bao tồn tại sinhvien
+        //ServiceRegistry lớp trừ tượng
+        ServiceRegistry registry= new StandardServiceRegistryBuilder().applySettings(conf.getProperties()).build();
+        FACTORY = conf.buildSessionFactory(registry);
+
+    }
+    public static SessionFactory getFactory() {
+        return FACTORY;
+    }
+
+}
